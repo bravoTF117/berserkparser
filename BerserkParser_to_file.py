58792e9d2ffd68a612d209a06befc42bcea12e55
JB <signup.mails.box@gmail.com>
2020-05-06 00:07:24 +0200

    remove unused def

--- BerserkParser_to_file.py
+++ BerserkParser_to_file.py
@@ -266,21 +266,6 @@ def main():
     print('\n','\n')
     print('Done... you can go check '+outfilename +'\n')
 
-def getListOfFiles(dirName):
-    # create a list of file and sub directories 
-    # names in the given directory 
-    listOfFile = os.listdir(dirName)
-    allFiles = list()
-    # Iterate over all the entries
-    for entry in listOfFile:
-        # Create full path
-        fullPath = os.path.join(dirName, entry)
-        # If entry is a directory then get the list of files in this directory 
-        if os.path.isdir(fullPath):
-            allFiles = allFiles + getListOfFiles(fullPath)
-        else:
-            allFiles.append(fullPath)
-    return allFiles
                 
 if __name__ == '__main__':
     main()
--- BerserkParser_to_screen.py
+++ BerserkParser_to_screen.py
@@ -243,22 +243,6 @@ def main():
     print('\n..............................................................................................................')
     print('Done @ ' + str(datetime.datetime.now()))
     print()
-
-def getListOfFiles(dirName):
-    # create a list of files and sub directories names in the given directory 
-    listOfFile = os.listdir(dirName)
-    allFiles = list()
-    # Iterate over all the entries
-    for entry in listOfFile:
-        # Create full path
-        fullPath = os.path.join(dirName, entry)
-        # If entry is a directory then get the list of files in this directory 
-        if os.path.isdir(fullPath):
-            allFiles = allFiles + getListOfFiles(fullPath)
-        else:
-            allFiles.append(fullPath)
-    return allFiles       
-
                 
 if __name__ == '__main__':
     main()
